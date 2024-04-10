f5d32fefc13fe709491c9ea6886a90792163c62d
fre-denni <fredenni.git@gmail.com>
2024-04-10 15:46:13 +0200

    adjusted responsive layout

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,4 @@
-document.querySelector('.menu-icons').addEventListener('click', () => {
-    document.querySelector('.menu-items').classList.toggle('expanded');
-  });
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.menu').classList.toggle('expanded');
+    document.querySelector('.hamburger').classList.toggle('open');
+});

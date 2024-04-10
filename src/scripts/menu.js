4d68c6acd85ccaa14cc361b0d704e19982fb9742
fre-denni <fredenni.git@gmail.com>
2024-04-10 18:56:41 +0200

    added viewtransition

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,6 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.menu').classList.toggle('expanded');
-    document.querySelector('.hamburger').classList.toggle('open');
+document.addEventListener('astro:page-load', () => {
+    document.querySelector('.hamburger').addEventListener('click', () => {
+        document.querySelector('.menu').classList.toggle('expanded');
+        document.querySelector('.hamburger').classList.toggle('open');
+    });
 });

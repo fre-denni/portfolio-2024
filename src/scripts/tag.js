2ef578f29ec403fe0ffcdb83ee5ecd9f6585f392
fre-denni <fredenni.git@gmail.com>
2024-05-03 14:11:04 +0200

    minor fixes

--- src/scripts/tag.js
+++ src/scripts/tag.js
@@ -4,7 +4,7 @@ document.addEventListener('astro:page-load', () => {
 
     buttons.forEach(button => { button.addEventListener('click', toggleTag);});
 
-    let selectedTags = ['All']; // starts with 'All' selected
+    let selectedTags = ['All Work']; // starts with 'All' selected
 
     function toggleTag(event) {
         let button = event.target;
@@ -20,23 +20,23 @@ document.addEventListener('astro:page-load', () => {
         } else {
             button.classList.remove('tag-unselected');
             button.classList.add('tag-selected');
-            if(tagName !== 'All') {
+            if(tagName !== 'All Work') {
                 selectedTags.push(tagName);
             }
         }
 
-        // If 'All' is selected or no other tags are selected, select 'All' and deselect others
-        if(tagName === 'All' || selectedTags.length === 0) {
-            selectedTags = ['All'];
+        // If 'All Work' is selected or no other tags are selected, select 'All' and deselect others
+        if(tagName === 'All Work' || selectedTags.length === 0) {
+            selectedTags = ['All Work'];
             buttons.forEach(button => {
                 button.classList.remove('tag-selected');
                 button.classList.add('tag-unselected');
             });
             allButton.classList.remove('tag-unselected');
             allButton.classList.add('tag-selected');
-        } else if(selectedTags.includes('All')) {
-            // If 'All' is selected and another tag is clicked, deselect 'All'
-            let index = selectedTags.indexOf('All');
+        } else if(selectedTags.includes('All Work')) {
+            // If 'All Work' is selected and another tag is clicked, deselect 'All'
+            let index = selectedTags.indexOf('All Work');
             if(index > -1) {
                 selectedTags.splice(index, 1);
             }
@@ -47,7 +47,7 @@ document.addEventListener('astro:page-load', () => {
         // If all other tags are selected, deselect them and select 'All'
         let nonAllButtons = Array.from(buttons).slice(1); // exclude "All" button
         if(nonAllButtons.every(button => button.classList.contains('tag-selected'))) {
-            selectedTags = ['All'];
+            selectedTags = ['All Work'];
             nonAllButtons.forEach(button => {
                 button.classList.remove('tag-selected');
                 button.classList.add('tag-unselected');

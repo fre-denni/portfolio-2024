8dab10974aee81176bb153bd22bad1891c54d4dd
fre-denni <fredenni.git@gmail.com>
2024-05-04 19:57:43 +0200

    minor adjustment requires major fix

--- src/scripts/projectsfilter.js
+++ src/scripts/projectsfilter.js
@@ -1,27 +1,27 @@
 document.addEventListener('astro:page-load', () => {
-function hideBlogs(evt) {
-    let selectedTag = evt.currentTarget.getAttribute("data-type");
-    let tagElement = document.getElementById("tags");
-    let tags = tagElement.dataset.tags;
-    tags = tags.split(",");
-    tags.splice(tags.indexOf(selectedTag), 1);
+    // Event listener for the button click
+    const input = document.querySelectorAll("#blogFilter");
+    input.forEach((e) => {
+        e.addEventListener("click", hideBlogs);
+    })
 
-    tags.forEach((t) => {
-      const elementsToHide = document.getElementsByClassName(t);
-      for (let i = 0; i < elementsToHide.length; i++) {
-        elementsToHide[i].style.display = "none";
-      }
-    });
+    function hideBlogs(evt) {
+        let selectedTag = evt.currentTarget.getAttribute("data-type");
+        let tagElement = document.getElementById("tags");
+        let tags = tagElement.dataset.tags;
+        tags = tags.split(",");
+        tags.splice(tags.indexOf(selectedTag), 1);
 
-    let elementsToShow = document.getElementsByClassName(selectedTag);
-    for (let i = 0; i < elementsToShow.length; i++) {
-      elementsToShow[i].style.display = "block";
+        tags.forEach((t) => {
+        const elementsToHide = document.getElementsByClassName(t);
+        for (let i = 0; i < elementsToHide.length; i++) {
+            elementsToHide[i].style.display = "none";
+        }
+        });
+
+        let elementsToShow = document.getElementsByClassName(selectedTag);
+        for (let i = 0; i < elementsToShow.length; i++) {
+        elementsToShow[i].style.display = "block";
+        }
     }
-  }
-
-  // Event listener for the button click
-  const input = document.querySelectorAll("#blogFilter");
-  input.forEach((e) => {
-    e.addEventListener("click", hideBlogs);
-  })
 });

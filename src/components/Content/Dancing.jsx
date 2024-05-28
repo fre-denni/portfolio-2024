c1156e27ec4b7b9bbcbb3d8bb40b92cae8e2b4c8
fre-denni <fredenni.git@gmail.com>
2024-05-28 16:21:32 +0200

    added p5.js sketch and react component

---
+++ src/components/Content/Dancing.jsx
@@ -0,0 +1,176 @@
+import React, { useEffect } from 'react';
+import p5 from 'p5';
+import toxi from 'toxiclibsjs';
+
+function Man() {
+    
+  const canvasRef = React.useRef(null);
+
+  useEffect(() => {
+
+    const sketch = (p) => {
+
+    // "Importing" the classes we need
+    let { Vec2D, Rect } = toxi.geom;
+    let { VerletPhysics2D, VerletParticle2D, VerletSpring2D, VerletMinDistanceSpring2D } = toxi.physics2d;
+    let { GravityBehavior } = toxi.physics2d.behaviors;
+
+    let physics;
+    let skeleton;
+    let img;
+
+    p.preload = function() {
+    img = p.loadImage("./me.png");
+    }
+
+    class Particle extends VerletParticle2D {
+        constructor(x, y, r) {
+        super(x, y);
+        this.r = r;
+        physics.addParticle(this);
+        }
+
+        show() {
+        fill(0);
+        circle(this.x, this.y, this.r * 2);
+        }
+    }
+
+    class Skeleton {
+        constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.particles = [];
+        this.springs = [];
+        this.hsprings = [];
+
+        this.head = new Particle(new Vec2D(x, y), 16);
+        let neck = new Particle(new Vec2D(x, y + 20), 4);
+        let lShoulder = new Particle(new Vec2D(x + 20, y + 80), 4);
+        let rShoulder = new Particle(new Vec2D(x - 20, y + 80), 4);
+        let lElbow = new Particle(new Vec2D(x + 100, y + 80), 8);
+        let rElbow = new Particle(new Vec2D(x - 100, y + 80), 8);
+        let lHand = new Particle(new Vec2D(x + 200, y + 80), 16);
+        let rHand = new Particle(new Vec2D(x - 200, y + 80), 16);
+        let belly = new Particle(new Vec2D(x, y + 120), 4);
+        let lKnee = new Particle(new Vec2D(x - 50, y + 230), 4);
+        let rKnee = new Particle(new Vec2D(x + 50, y + 230), 4);
+        let lFoot = new Particle(new Vec2D(x + 50, y + 280), 16);
+        let rFoot = new Particle(new Vec2D(x + 50, y + 280), 16);
+
+        this.springs.push(new Spring(this.head, neck, 5, 0.05));
+        this.springs.push(new Spring(neck, lShoulder, 20, 0.05));
+        this.springs.push(new Spring(neck, rShoulder, 20, 0.05));
+        this.springs.push(new Spring(rShoulder, rElbow, 40, 0.05));
+        this.springs.push(new Spring(lShoulder, lElbow, 40, 0.05));
+        this.hsprings.push(new SpringMin(rShoulder, lShoulder, 100, 0.05));
+
+        this.springs.push(new Spring(rHand, rElbow, 10, 0.05));
+        this.springs.push(new Spring(lHand, lElbow, 10, 0.05));
+        this.springs.push(new Spring(neck, belly, 50, 0.05));
+        this.hsprings.push(new SpringMin(rShoulder, belly, 50, 0.05));
+        this.hsprings.push(new SpringMin(lShoulder, belly, 50, 0.05));
+        this.hsprings.push(new SpringMin(lShoulder, this.head, 50, 0.05));
+        this.hsprings.push(new SpringMin(rShoulder, this.head, 50, 0.05));
+        this.springs.push(new Spring(belly, lKnee, 80, 0.05));
+        this.springs.push(new Spring(belly, rKnee, 80, 0.05));
+        this.hsprings.push(new SpringMin(lKnee, rKnee, 50, 0.05));
+        this.springs.push(new Spring(lFoot, lKnee, 80, 0.05));
+        this.springs.push(new Spring(rFoot, rKnee, 80, 0.05));
+        this.head.lock();
+        }
+
+        dance() {
+        this.head.x = this.x + sin(frameCount * 0.25) * 16;
+        this.head.y = this.y + cos(frameCount * 0.25) * 16;
+        }
+
+        show() {
+        imageMode(CENTER);
+        for (let s of this.springs) {
+            s.show();
+        }
+        image(img, this.head.x, this.head.y - 25, 50, 70);
+        }
+    }
+
+
+    class Spring extends VerletSpring2D {
+        constructor(a, b, len, strength) {
+            super(a, b, len, strength);
+            physics.addSpring(this);
+        }
+        
+        show() {
+            stroke(112, 50, 126);
+            strokeWeight(8);
+        
+            line(this.a.x, this.a.y, this.b.x, this.b.y);
+        }
+        }
+        
+        class SpringMin extends VerletMinDistanceSpring2D {
+        constructor(a, b, len, strength) {
+            super(a, b, len, strength);
+            physics.addSpring(this);
+        }
+        
+        show() {
+            stroke(0);
+            strokeWeight(0.5);
+            line(this.a.x, this.a.y, this.b.x, this.b.y);
+        }
+        }
+        
+        p.setup = function() {
+            let canvas = p.createCanvas(p.windowWidth, p.windowHeight);
+            canvas.parent(canvasRef.current);
+
+            // Make the world
+            physics = new VerletPhysics2D();
+        
+            // Adding gravity to the world
+            let gravity = new GravityBehavior(new Vec2D(0, 0.1));
+            physics.addBehavior(gravity);
+        
+            // Add a bounds
+            let boundary = new Rect(0, 0, p.width, p.height);
+            physics.setWorldBounds(boundary);
+        
+            skeleton = new Skeleton(p.width / 2, 100);
+        }
+
+        p.draw = function() {
+            p.background(240, 99, 164);
+            physics.update();
+            skeleton.show();
+            skeleton.dance();
+        }
+        
+        p.windowResized = function() {
+            p.resizeCanvas(p.windowWidth, p.windowHeight);
+        }
+        }
+        
+        // Create a new p5 instance and attach it to your canvasRef
+        let myp5 = new p5(sketch, canvasRef.current);
+
+        // Return a cleanup function to remove the sketch when the component unmounts
+        return () => myp5.remove();
+
+    }, []);
+
+    return (
+        <div ref={canvasRef} style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            height: '600px',
+            overflow: 'hidden'
+            }}>
+        </div>
+    );
+}
+
+export default Man;
